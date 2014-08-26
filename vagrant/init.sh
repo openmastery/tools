5644916d317c117ce287db6bc4021f1b43a13c61
Janelle Klein <janelle@newiron.com>
2014-08-25 22:16:56 -0500

    switched to wget to fix hangup issue

--- vagrant/init.sh
+++ vagrant/init.sh
@@ -4,7 +4,7 @@ sudo apt-get -y update
 sudo apt-get -y install openjdk-7-jre-headless
 
 rm -f visualizer.jar
-curl -s http://ideaflow.org/visualizer-latest.jar > visualizer.jar
+wget --tries=10 --append-output=wget.out -O visualizer.jar http://ideaflow.org/visualizer-latest.jar
 
 echo "Stating visualizer..."
 java -jar visualizer.jar context=visualizer &
